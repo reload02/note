15f42b2132d69ccb744fa4c5a1a60c756648df89
김호경 <jackjack0@naver.com>
2024-10-21 12:09:46 +0900

    .

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,10 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import "./Note.css";
 
-function convertDate(date) {
-  let a = "asdsd";
-  if (date.length > 14) return date;
-}
 
 const Note = ({ note }) => {
   const nav = useNavigate();
@@ -17,7 +13,9 @@ const Note = ({ note }) => {
     >
       <span>{note.title} </span>
       <span className="date">
-        {note.Editdate ? note.Editdate + " 수정됨" : note.Newdate + " 원본"}
+        {note.Editdate
+          ? note.Editdate + " 수정됨"
+          : note.Newdate + " 원본"}
       </span>
     </div>
   );
